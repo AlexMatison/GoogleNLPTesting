19f8563c6a11caf0714ff73b9970c82e26092db1
AlexMatison <AlexMatison@users.noreply.github.com>
2017-10-22 20:57:49 +1030

    no comment

--- src/main/java/com/mycompany/googlenlptesting/NLMSomething.java
+++ src/main/java/com/mycompany/googlenlptesting/NLMSomething.java
@@ -137,13 +137,13 @@ public class NLMSomething {
             //System.out.printf("Salience: %.3f\n", entity.getSalience());
             //System.out.printf("Type: %s\n", entity.getType());
             //System.out.println("Metadata: \n");
-            for (Map.Entry<String, String> entry : entity.getMetadataMap().entrySet()) {
-                //System.out.printf("%s : %s\n", entry.getKey(), entry.getValue());
-                if (entry.getKey() == "wikipedia_url") {
-                    wikipediaUrl[wikipediaCount] = entry.getValue();
-                    wikipediaCount++;
-                }
-            }
+//            for (Map.Entry<String, String> entry : entity.getMetadataMap().entrySet()) {
+//                //System.out.printf("%s : %s\n", entry.getKey(), entry.getValue());
+//                if (entry.getKey() == "wikipedia_url") {
+//                    wikipediaUrl[wikipediaCount] = entry.getValue();
+//                    wikipediaCount++;
+//                }
+//            }
             //for (EntityMention mention : entity.getMentionsList()) {
             //    System.out.printf("Begin offset: %d\n", mention.getText().getBeginOffset());
             //    System.out.printf("Content: %s\n", mention.getText().getContent());
